67696775fce7cbcdc9cabe3d5e41554bb0ad529e
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-30 15:29:10 -0400

    Add typescript to janus

--- backend/src/janus/index.ts
+++ backend/src/janus/index.ts
@@ -1,51 +1,21 @@
 import { $ } from "bun";
 
-async function getJanusPath(): Promise<string> {
-  const fullPath = await $`which janus`.text();
-  return fullPath.replace("/bin/janus", "");
-}
-
-export async function startJanus(
-  configDirPath: string,
-  baseConfigPath: string,
-  path: string = "janus",
-) {
+async function startJanus(configDirPath: string, baseConfigPath: string) {
+  const janusRoot = (await $`which janus`.text()).replace("/bin/janus", "");
   return Bun.spawn(
     [
-      path,
+      `${janusRoot}/bin/janus`,
       "-P",
-      `${await getJanusPath()}/lib/janus/plugins`,
+      `${janusRoot}/lib/janus/plugins`,
       "-F",
       configDirPath,
       "-C",
       baseConfigPath,
     ],
     {
-      onExit: (proc, exitCode, signalCode, error) =>
-        console.log("Janus Exited", proc, exitCode, signalCode, error),
+      stdout: "inherit",
     },
   );
 }
 
-export function startFfmpegStream(path = "ffmpeg") {
-  return Bun.spawn([
-    path,
-    "-re",
-    "-f",
-    "h264",
-    "-i",
-    "tcp://127.0.0.1:1234",
-    "-an",
-    "-g",
-    "10",
-    "-c:v",
-    "copy",
-    "-f",
-    "rtp",
-    "-loglevel",
-    "verbose",
-    "-sdp_file",
-    "video.sdp",
-    "rtp://127.0.0.1:5004",
-  ]);
-}
+await startJanus("configs", "janus.jcfg");
