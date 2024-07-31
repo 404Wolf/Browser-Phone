1b16e4bac51895f421b65357c1b004f51915853e
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-31 17:44:12 -0400

    Update gitignore

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Android in the browser with WebRTC and Nixified Android";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
+        };
+      in {
+        packages = {
+          default = pkgs.callPackage ./server.nix {inherit pkgs;};
+          run-janus-gateway = pkgs.callPackage ./src/janus {inherit pkgs;};
+          build-android-emulator = args:
+            import ./src/android/emulator.nix {
+              args = builtins.fromJSON args;
+              inherit pkgs;
+            };
+        };
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bun
+            pkgs.vlc
+            pkgs.tcpdump
+            pkgs.scrcpy
+            pkgs.janus-gateway
+            pkgs.ffmpeg_7-full
+            pkgs.android-tools
+            pkgs.android-studio
+          ];
+        };
+      }
+    );
+}
